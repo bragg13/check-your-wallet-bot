fdb12ac454b6506b292ef7657447d90d3894de2d
Andrea Bragante <andrea.bragante00@gmail.com>
2025-02-15 17:27:58 +0100

    more europe in those currencies

--- src/Expenses/expense.js
+++ src/Expenses/expense.js
@@ -1,37 +1,48 @@
 import { Bot, Context, Keyboard } from "grammy";
 import { calendarMenu, mainKeyboard } from "../../index.js";
 import { formatMoney, sortByDate, currencySymbols } from "../Utils/handlers.js";
-import { categoryKeyboard, expenseKeyboard, customCategoryKeyboard } from "../Expenses/expenseKeyboards.js";
+import {
+  categoryKeyboard,
+  expenseKeyboard,
+  customCategoryKeyboard,
+} from "../Expenses/expenseKeyboards.js";
 import { run } from "grammy/out/composer.js";
 
 export async function expenseHandler(conversation, ctx) {
   let expense = {};
   let money, note, category, date;
-  let noteDone = false, categoryDone = false, dateDone = false, done = false;
+  let noteDone = false,
+    categoryDone = false,
+    dateDone = false,
+    done = false;
   date = new Date();
 
-  await ctx.reply(`Sad to hear that ${conversation.session.user.username} :c\nHow much did you spend?\n(press 'q' to cancel))`);
+  await ctx.reply(
+    `Sad to hear that ${conversation.session.user.username} :c\nHow much did you spend?\n(press 'q' to cancel))`,
+  );
   ctx = await conversation.wait();
 
   // check if it is a number
   while (isNaN(ctx.message.text)) {
     // quit if user types 'q'
-    if (ctx.message.text == 'q') {
+    if (ctx.message.text == "q") {
       await ctx.reply(`Canceling operation...`, {
-        reply_markup: mainKeyboard()
-      })
+        reply_markup: mainKeyboard(),
+      });
       return;
     }
 
-    await ctx.reply(`Are you sure that is a number? (ndr. use dot as separator)`);
+    await ctx.reply(
+      `Are you sure that is a number? (ndr. use dot as separator)`,
+    );
     ctx = await conversation.wait();
   }
 
   // quit if user inputs 0
-  if (ctx.message.text == '0') {
+  if (ctx.message.text == "0") {
     await ctx.reply(`Canceling operation...`, {
-      reply_markup: mainKeyboard()
-    })
+      reply_markup: mainKeyboard(),
+    });
     return;
   }
 
@@ -43,47 +54,61 @@ export async function expenseHandler(conversation, ctx) {
     // loop to fill out the expense note
     let kb = expenseKeyboard(noteDone, categoryDone, dateDone);
 
-    await ctx.reply((noteDone || categoryDone || dateDone) ? `...only ${money}${currencySymbol}? Don't make it a big deal` : `Only ${money}${currencySymbol}? Don't make it a big deal`, {
-      reply_markup: kb
-    });
+    await ctx.reply(
+      noteDone || categoryDone || dateDone
+        ? `...only ${money}${currencySymbol}? Don't make it a big deal`
+        : `Only ${money}${currencySymbol}? Don't make it a big deal`,
+      {
+        reply_markup: kb,
+      },
+    );
 
     // what next?
     ctx = await conversation.wait();
     switch (ctx.message.text) {
-      case '(✔️) 🗒️ Add some notes...':
-      case '🗒️ Add some notes...':
+      case "(✔️) 🗒️ Add some notes...":
+      case "🗒️ Add some notes...":
         // ask to input some text
-        await ctx.reply(`So, WHY did you spend those ${money}${currencySymbol}?`);
+        await ctx.reply(
+          `So, WHY did you spend those ${money}${currencySymbol}?`,
+        );
         ctx = await conversation.wait();
 
         note = ctx.message.text;
         noteDone = true;
         break;
 
-      case '(✔️) 🏺 Set category...':
-      case '🏺 Set category...':
+      case "(✔️) 🏺 Set category...":
+      case "🏺 Set category...":
         // ask to input some text
-        await ctx.reply(`So, HOW did you spend those ${money}${currencySymbol}?`, {
-          reply_markup: categoryKeyboard()
-        });
+        await ctx.reply(
+          `So, HOW did you spend those ${money}${currencySymbol}?`,
+          {
+            reply_markup: categoryKeyboard(),
+          },
+        );
         ctx = await conversation.wait();
 
         // custom category
-        if (ctx.message.text == '🫠 Custom...') {
-          
-          let _msg = (conversation.session.user.custom_categories.length === 0)
-          ? `You have no custom category yet. You can add one by typing it in the chat.`
-          : `These are your custom categories:`
+        if (ctx.message.text == "🫠 Custom...") {
+          let _msg =
+            conversation.session.user.custom_categories.length === 0
+              ? `You have no custom category yet. You can add one by typing it in the chat.`
+              : `These are your custom categories:`;
 
           // inline keyboard o text
           await ctx.reply(_msg, {
-            reply_markup: customCategoryKeyboard(conversation.session.user.custom_categories)
+            reply_markup: customCategoryKeyboard(
+              conversation.session.user.custom_categories,
+            ),
           });
 
           ctx = await conversation.wait();
 
           // add custom category to user session
-          if (!(ctx.message.text in conversation.session.user.custom_categories)) {
+          if (
+            !(ctx.message.text in conversation.session.user.custom_categories)
+          ) {
             conversation.session.user.custom_categories.push(category);
           }
         }
@@ -91,45 +116,44 @@ export async function expenseHandler(conversation, ctx) {
         categoryDone = true;
         break;
 
-      case '(✔️) 📅 Set date...':
-      case '📅 Set date...':
-
+      case "(✔️) 📅 Set date...":
+      case "📅 Set date...":
         conversation.session.calendarOptions = { defaultDate: new Date() };
-        await ctx.reply(`So, WHEN did you spend those ${money}${currencySymbol}?`, {
-          reply_markup: calendarMenu
-        });
+        await ctx.reply(
+          `So, WHEN did you spend those ${money}${currencySymbol}?`,
+          {
+            reply_markup: calendarMenu,
+          },
+        );
         ctx = await conversation.wait();
         date = ctx.calendarSelectedDate;
         dateDone = true;
         break;
 
-      case '✅ Done.':
+      case "✅ Done.":
         done = true;
         break;
-
     }
 
-    if (noteDone && categoryDone)
-      done = true;
+    if (noteDone && categoryDone) done = true;
   }
 
   // set the expense object
-  expense['type'] = 'expense';
-  expense['money'] = money;
-  expense['currency'] = ctx.session.user.def_currency;
-  if (noteDone) expense['note'] = note;
-  if (categoryDone) expense['category'] = category;
-  expense['date'] = date;
+  expense["type"] = "expense";
+  expense["money"] = money;
+  expense["currency"] = ctx.session.user.def_currency;
+  if (noteDone) expense["note"] = note;
+  if (categoryDone) expense["category"] = category;
+  expense["date"] = date;
 
   // save to session sorting wallet arra
   conversation.session.user.wallet.push(expense);
-  conversation.session.user.wallet = sortByDate(conversation.session.user.wallet);
+  conversation.session.user.wallet = sortByDate(
+    conversation.session.user.wallet,
+  );
 
   await ctx.reply(`➕ Expense added!`, {
-    reply_markup: mainKeyboard()
-  })
+    reply_markup: mainKeyboard(),
+  });
   return;
 }
-
-
-
--- src/handlers.js
+++ src/handlers.js
@@ -2,86 +2,89 @@ import { Bot, Context, Keyboard } from "grammy";
 import { calendarMenu, mainKeyboard } from "../index.js";
 
 export const currencySymbols = {
-  'EUR': '€',
-  'USD': '$',
-  'JPY': '¥',
-  'GBP': '£',
-  'AUD': '$',
-  'CAD': '$',
-  'CHF': '₣',
-  'CNH': '¥'
+  EUR: "€",
+  USD: "$",
+  GBP: "£",
+  PLN: "zł",
+  DKK: "kr.",
+  SEK: "kr",
 };
 
 export const months = {
-  0: 'January',
-  1: 'February',
-  2: 'March',
-  3: 'April',
-  4: 'May',
-  5: 'June',
-  6: 'July',
-  7: 'August',
-  8: 'September',
-  9: 'October',
-  10: 'November',
-  11: 'December'
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
 };
 
 const settingsKb = (settings) => {
   let kb = new Keyboard();
-  kb.text(settings.monthlySumup ? '🔕 Set monthly sum-up off' : '🔔 Set monthly sum-up on').row();
-  kb.text('❌ Delete my data').row();
-  kb.text('🔙 Back 🔙').row();
+  kb.text(
+    settings.monthlySumup
+      ? "🔕 Set monthly sum-up off"
+      : "🔔 Set monthly sum-up on",
+  ).row();
+  kb.text("❌ Delete my data").row();
+  kb.text("🔙 Back 🔙").row();
 
   return kb.oneTime();
-}
-
+};
 
 const currencyKb = () => {
   let kb = new Keyboard();
-  kb.text('EUR').text('USD').row();
-  kb.text('GBP').text('JPY').row();
-  kb.text('AUD').text('CAD').row();
-  kb.text('CHF').text('CNH').row();
-  
+  kb.text("EUR").text("USD").row();
+  kb.text("GBP").text("PLN").row();
+  kb.text("DKK").text("SEK").row();
+
   return kb.oneTime();
-}
+};
 
 export const sortByDate = (array) => {
   let _array = array.sort((a, b) => a.date - b.date);
   return _array;
-}
+};
 
-export const formatMoney = money => {
-  let _money = money.toString().split('.');
+export const formatMoney = (money) => {
+  let _money = money.toString().split(".");
 
-  return (_money.length == 1)
-    ? money                                       // no digits after dot, return
-    : (_money[1].length == 1)
-      ? `${_money[0]}.${_money[1]}0`              // one digit after dot, add a 0
-      : money                                     // two digits after dot, return
-}
+  return _money.length == 1
+    ? money // no digits after dot, return
+    : _money[1].length == 1
+      ? `${_money[0]}.${_money[1]}0` // one digit after dot, add a 0
+      : money; // two digits after dot, return
+};
 
 const deleteData = (conversation) => {
   conversation.session.user = {
-    chatid: '',
-    username: '',
-    lang: '',
-    def_currency: 'EUR',
+    chatid: "",
+    username: "",
+    lang: "",
+    def_currency: "EUR",
     expenses: [],
     incomes: [],
     settings: {
-      monthlySumup: true
-    }
+      monthlySumup: true,
+    },
   };
 
-  console.log('tbd');
-}
+  console.log("tbd");
+};
 
 export async function currencyHandler(conversation, ctx) {
-  await ctx.reply('Okay, choose a currency from below or type in a custom one:', {
-    reply_markup: currencyKb()
-  });
+  await ctx.reply(
+    "Okay, choose a currency from below or type in a custom one:",
+    {
+      reply_markup: currencyKb(),
+    },
+  );
 
   ctx = await conversation.wait();
 
@@ -91,8 +94,8 @@ export async function currencyHandler(conversation, ctx) {
 
   // back
   await ctx.reply(`New currency set!`, {
-    reply_markup: mainKeyboard()
-  })
+    reply_markup: mainKeyboard(),
+  });
   return;
 }
 
@@ -100,14 +103,14 @@ export async function settingsHandler(conversation, ctx) {
   let done = false;
   let curr;
   const yesnoKb = new Keyboard();
-  yesnoKb.text('YES!').row();
-  yesnoKb.text('Oops wrong button').row();
+  yesnoKb.text("YES!").row();
+  yesnoKb.text("Oops wrong button").row();
   yesnoKb.oneTime();
 
   let settings = conversation.session.user.settings;
 
   await ctx.reply(`Select an option:`, {
-    reply_markup: settingsKb(settings)
+    reply_markup: settingsKb(settings),
   });
 
   while (!done) {
@@ -115,46 +118,43 @@ export async function settingsHandler(conversation, ctx) {
     ctx = await conversation.wait();
 
     switch (ctx.message.text) {
-      case '🔕 Set monthly sum-up off':
-      case '🔔 Set monthly sum-up on':
+      case "🔕 Set monthly sum-up off":
+      case "🔔 Set monthly sum-up on":
         curr = conversation.session.user.settings.monthlySumup;
         conversation.session.user.settings.monthlySumup = !curr;
 
         await ctx.reply(`Select an option:`, {
-          reply_markup: settingsKb(settings)
+          reply_markup: settingsKb(settings),
         });
 
-      case '❌ Delete my data':
+      case "❌ Delete my data":
         await ctx.reply(`Are you sure you want to delete all your data?`, {
-          reply_markup: yesnoKb
-        })
+          reply_markup: yesnoKb,
+        });
 
         ctx = await conversation.wait();
 
-        if (ctx.message.text == 'YES!') {
+        if (ctx.message.text == "YES!") {
           deleteData(conversation);
           await ctx.reply(`Data successfully deleted!`);
           done = true;
-
         } else {
           await ctx.reply(`Happens all the time.`, {
-            reply_markup: settingsKb(settings)
+            reply_markup: settingsKb(settings),
           });
-
         }
         break;
 
-      case '🔙 Back 🔙':
+      case "🔙 Back 🔙":
         done = true;
         break;
-
     }
   }
 
   // back
   await ctx.reply(`Back to main menu...`, {
-    reply_markup: mainKeyboard()
-  })
+    reply_markup: mainKeyboard(),
+  });
   return;
 }
 
